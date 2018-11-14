518cf54e8b081039f6a1a75d00e5493e792240aa
admin <admin@LAPTOP-ROMDGHH2>
2018-11-14 09:27:19 +0800

    first build

---
+++ src/main/java/com/bwf/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.bwf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("test")
+	public void test(){
+		System.out.println(  "test controller" );
+		int a;
+	}
+	
+}
